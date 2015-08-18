9ff1e61f5284d5bfd1090d06cb824e1b85e728d0
alexinder <alexinder@live.com>
2015-08-18 12:24:38 +0100

    Saving logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,17 +1,6 @@
 from tealight.logo import move, turn, color
 
 
-def drawsquare():
-  turn(90)
-  move(25)
-  turn(90)
-  move(25)
-  turn(90)
-  move(25)
-  turn(90)
-  move(25)
-  
-
 def oneline():
   for i in range(0,4):
     turn(90)
