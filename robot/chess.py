fb5877009c90593c51b1aeeaee2d72ce10ddedb0
alexinder <alexinder@live.com>
2015-08-18 14:58:30 +0100

    Saving robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -15,6 +15,12 @@ def turnrightand4():
   distance = 4
   for n in range(0,distance):
     move()
+    
+def turnleftand4():
+  turn(1) 
+  distance = 4
+  for n in range(0,distance):
+    move()
 
 move32()
 turnand4()
