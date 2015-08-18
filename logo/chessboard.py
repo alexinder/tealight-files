28f408f4b32fd86cda999e70300ed6f17221ac35
alexinder <alexinder@live.com>
2015-08-18 12:12:21 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,15 +1,15 @@
 from tealight.logo import move, turn, color
 
 
-
-turn(90)
-move(25)
-turn (90)
-move (25)
-turn (90)
-move (25)
-turn (90)
-move (25)
+def drawsquare():
+  turn(90)
+  move(25)
+  turn (90)
+  move (25)
+  turn (90)
+  move (25)
+  turn (90)
+  move (25)
 
 turn (90)
 move(25)
