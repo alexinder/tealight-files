c1b90b3bc9bfb73eb8294f0a8020937a20badae6
alexinder <alexinder@live.com>
2015-08-18 14:54:02 +0100

    Saving robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -5,8 +5,10 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-
+def move32():
 distance = 32
 for n in range(0,distance):
   move()
+  
+
 
