7ac3e9e6e295afec4270c92cf27f3f05cfa69638
alexinder <alexinder@live.com>
2015-08-20 13:41:55 +0100

    Running art/alexpaint.py

--- art/alexpaint.py
+++ art/alexpaint.py
@@ -19,7 +19,7 @@ h= 25
 DrawPalette(x,y, colors , w, h)
 
 def handle_mousedown(mx,my):
-  if mx > x and mx < x+w and my > y and my < y+h:
+  if mx > x and mx < x+w and my > y and my < y+(h*len(colors)):
     
       RowNo = (my-y)/h
   
