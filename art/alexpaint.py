281bf349c45bc88e463cd72ddf6179dca050b387
alexinder <alexinder@live.com>
2015-08-20 11:47:53 +0100

    Running art/alexpaint.py

--- art/alexpaint.py
+++ art/alexpaint.py
@@ -1,24 +1,5 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-lastx = 0
-lasty = 0
-
-color("purple")
-
-def handle_mousedown(x,y):
-  global lastx, lasty
-  
-  lastx = x
-  lasty = y
-
-def handle_mousemove(x,y,button):
-  global lastx, lasty
-  
-  if button == "left":
-    line(lastx, lasty, x, y)
-    lastx = x
-    lasty = y
-
 color("black")
 box(10, 75, 25, 25)
 
